f92f8c8d50c2fdcc7788068ccc50f1ef4bf92490
moses <moses.kamau@student.moringaschool.com>
2023-06-19 23:57:06 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const saturdayFun = function(defaultActivity = 'roller-skate'){
+    return "This Saturday, I want to roller-skate!";
+};
+
+saturdayFun("Bathe my dog");
+const mondayWork = function(x = 'go to the office.'){ 
+    return 'This Monday, I will ' + x;
+}
+function wrapAdjective(){
+    return "You are *a hard worker*!";
+}
+
